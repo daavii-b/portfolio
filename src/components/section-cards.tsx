cf6e7a16a58d1611b923f2842f9d152e03159d18
daaviib <davi.santos@ax4b.com>
2025-11-03 15:17:02 -0300

    feat: ✨ improve section card

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -1,5 +1,4 @@
-import QrCode from "@/assets/qrcode-svg.svg";
-import { cn } from "@/lib";
+import { cn, ENV_VARIABLES } from "@/lib";
 import {
   CornerRightDown,
   LucideActivity,
@@ -8,7 +7,7 @@ import {
   LucideLinkedin,
   LucideMessageCircleMore,
 } from "lucide-react";
-import { ComponentProps } from "react";
+import type { ComponentProps } from "react";
 import { Container } from "./container";
 import { Button, Card, CardContent, CardHeader, CardTitle } from "./ui";
 
@@ -24,22 +23,26 @@ export const AboutCard = ({ ...props }: ComponentProps<typeof Card>) => {
         </SectionTitle>
       </CardHeader>
       <SectionContent>
-        <strong>
-          Fullstack Software Engineer with over 2 years of experience
-        </strong>{" "}
-        building scalable applications using TypeScript, Nest.JS, and React.{" "}
-        <strong>
-          Deep experience in backend system architecture (Clean Architecture,
-          SOLID, DDD), developing RESTful APIs
-        </strong>
-        , and processing asynchronous tasks (BullMQ).{" "}
-        <strong>Skilled in frontend development (React, Next.js)</strong> and
-        managing the full{" "}
-        <strong>
-          DevOps lifecycle, including CI/CD (GitHub Actions) and production
-          deployments to AWS (ECS, S3)
-        </strong>
-        .
+        <p>
+          I am a <strong>Fullstack Software Engineer</strong> with a proven
+          track record of over two years in building scalable applications using
+          <strong>TypeScript, Nest.JS, Next.Js and React</strong>.
+        </p>
+        <br />
+        <p>
+          I have extensive experience in{" "}
+          <strong>backend system architecture</strong>, including Clean
+          Architecture, SOLID, and DDD. In addition, I am skilled in developing
+          RESTful APIs in accordance with best practices, including SOLID, Clean
+          Code and Design Patterns.
+        </p>
+        <br />
+        <p>
+          In addition, I have experience of{" "}
+          <strong>front-end development</strong> using React, Next.js and
+          Tailwind. I have expertise in building robust and intuitive interfaces
+          for multiple devices, with a focus on optimizing the user experience.
+        </p>
       </SectionContent>
     </SectionCard>
   );
@@ -64,7 +67,11 @@ export const SocialCard = ({ ...props }: ComponentProps<typeof Card>) => {
         <Container className="flex w-fit flex-col gap-1">
           <WhatsAppLink />
           <Container className="w-32">
-            <img src={QrCode} className="max-w-full" />
+            <img
+              src={ENV_VARIABLES.QR_LINK}
+              aria-label="QRCode from WhatsApp"
+              className="max-w-full"
+            />
           </Container>
         </Container>
       </SectionContent>
@@ -76,7 +83,7 @@ const WhatsAppLink = () => {
   return (
     <SocialLink
       className="p-0 hover:text-emerald-600 dark:hover:text-emerald-100"
-      href="https://wa.link/10uvyb"
+      href={ENV_VARIABLES.WA_LINK}
     >
       <span>
         <LucideMessageCircleMore className="size-4 text-emerald-800 dark:text-emerald-300" />
@@ -91,10 +98,7 @@ const WhatsAppLink = () => {
 
 const LinkedInLink = () => {
   return (
-    <SocialLink
-      href="https://www.linkedin.com/in/daaviib"
-      aria-label="link for linkedin"
-    >
+    <SocialLink href={ENV_VARIABLES.LKDIN_LINK} aria-label="link for linkedin">
       <span>
         <LucideLinkedin className="size-4 text-blue-400" />
       </span>
@@ -105,7 +109,7 @@ const LinkedInLink = () => {
 
 const GithubLink = () => {
   return (
-    <SocialLink href="https://github.com/daavii-b" aria-label="link for github">
+    <SocialLink href={ENV_VARIABLES.GITHUB_LINK} aria-label="link for github">
       <span>
         <LucideGithub className="size-4 text-slate-400" />
       </span>
@@ -124,8 +128,11 @@ const SocialLink = ({ ...props }: ComponentProps<"a">) => {
     >
       <Button
         variant={"link"}
-        value={"https://www.linkedin.com/in/daaviib/"}
-        className={cn(props.className)}
+        value={props.href}
+        className={cn(
+          "underline hover:text-sky-700 dark:hover:text-sky-300",
+          props.className,
+        )}
       >
         {props.children}
       </Button>
