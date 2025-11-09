96c3d9a24e83e4719d92b8a1df560fd57eb0eebd
daaviib <davi.santos@ax4b.com>
2025-11-08 23:09:02 -0300

    feat: ✨ add new UI components

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,4 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./dropdown-menu";
+export * from "./input";
+export * from "./input-group";
 export * from "./navigation-menu";
+export * from "./tabs";
